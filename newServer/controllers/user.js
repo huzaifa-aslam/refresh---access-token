da158490a8d0e563ca888e5f8b9e90017afc8863
Muhammad Hammad <muhammad.hammad@markematicsdigital.com>
2023-05-03 17:24:28 +0500

    add error handling

--- newServer/controllers/user.js
+++ newServer/controllers/user.js
@@ -23,17 +23,17 @@ const createUser = async (request, response) => {
   }
 };
 const fetchUser = async (request, response) => {
+  debugger;
   try {
     // let token = request.headers.authorization.split(" ")[1];
     // const decoded = verify(token, process.env.ACCESS_TOKEN);
 
-    // console.log("token", token);
+    console.log("token", request.headers.authorization);
     let Users = await tUser.findAll();
 
     return response.status(201).json(Users);
   } catch (error) {
     return response.status(401).send("unauthorized");
-    throw error;
   }
 };
 module.exports = {
--- newServer/middlewares/auth.js
+++ newServer/middlewares/auth.js
@@ -3,15 +3,19 @@ const { sign, verify, TokenExpiredError } = require("jsonwebtoken");
 const authMiddleware = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    decodedData = verify(token, process.env.ACCESS_TOKEN);
-    if (!decodedData)
-      return response.status(401).json({
+    if (!token)
+      return res.status(401).json({
         message: "Unauthorized",
       });
+    decodedData = verify(token, process.env.ACCESS_TOKEN);
     console.log("token", token);
     console.log("decodedData", decodedData);
     next();
   } catch (error) {
+    res.status(401).json({
+      message: "Unauthorized",
+    });
+
     if (error instanceof TokenExpiredError) {
       return res.status(401).json({
         res: "Token expired",
